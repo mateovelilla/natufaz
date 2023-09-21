563302ddcd6769d9ff49d80a29d249b58f981648
mateovelilla <teovelilla@gmail.com>
2023-09-20 23:24:16 -0500

    ✨ So close to finish the header

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        pink: "#F5CAC3",
+        green: "#84A59D",
+        white: "#F7EDE2"
+      },
+      fontFamily: {
+        source: ["'Source Sans 3'"],
+      },
+    },
+  },
+  plugins: [],
+}
